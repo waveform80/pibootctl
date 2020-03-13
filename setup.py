b7883d4a76966602485d1cad59dfc6414f532f0c
Dave Jones <dave@waveform.org.uk>
2020-03-13 10:37:20 +0000

    Install requires setuptools for pkg_resources
    Also move setup_requires to setup.cfg

--- setup.py
+++ setup.py
@@ -4,9 +4,5 @@ setup(
     use_scm_version={
         'version_scheme': 'post-release',
         'local_scheme': 'dirty-tag'
-    },
-    setup_requires=[
-        'setuptools >= 36.2.7',
-        'setuptools_scm >= 1.7.0'
-    ],
+    }
 )
